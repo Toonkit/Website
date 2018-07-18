f5ebc0a0452043b95187467d70d50cc1a800c61c
Bin <jpahman2003@gmail.com>
2018-07-18 18:01:57 -0500

    Update tracker.js

--- js/tracker.js
+++ js/tracker.js
@@ -1,22 +1,22 @@
- setInterval(()=>{
- var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-    var element = $('#curinv');
-    var data = JSON.parse(xhttp.responseText);
-    var i = 0;
-    $.each(data, function (i, o) {
-    if (o.cogs_attacking != 'None') {
-var newElement = `<section id="${i%2==0?card-left:card-right}">
+ setInterval(() => {
+             var xhttp = new XMLHttpRequest();
+             xhttp.onreadystatechange = function() {
+                 if (this.readyState == 4 && this.status == 200) {
+                     var element = $('#curinv');
+                     var data = JSON.parse(xhttp.responseText);
+                     var i = 0;
+                     $.each(data, function(i, o) {
+                         if (o.cogs_attacking != 'None') {
+                             var newElement = `<section id="${i%2==0?'card-left':'card-right'}">
 <img draggable="false" src="https://toonhq.org/static/2.4.3/img/cogs/${o.cogs_attacking}.png">
                 <h2>${o.cogs_attacking}</h2>
                 <h3>${o.name}</h3>
                 <p>${o.count_defeated}/${o.count_total} Cogs | <b>${Math.round(o.remaining_time/60)}</b> minutes remaining</p>
  </section>`;
-element.insertAdjacentHTML('beforeend', newElement);
-        }
-});
-  };
-  xhttp.open("GET", "https://corporateclash.net/api/v1/districts", true);
-  xhttp.send();
-  },60000);
+                             element.insertAdjacentHTML('beforeend', newElement);
+                         }
+                     });
+                 };
+                 xhttp.open("GET", "https://corporateclash.net/api/v1/districts", true);
+                 xhttp.send();
+             }, 60000);
