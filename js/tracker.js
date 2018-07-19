8ccb8cb31018d96518213b99ec1c5ba994eb0ef9
Bin <jpahman2003@gmail.com>
2018-07-18 21:14:17 -0500

    Update tracker.js

--- js/tracker.js
+++ js/tracker.js
@@ -6,16 +6,18 @@ $(document).ready(function() {
             var element = $("#curinv")[0];
             var data = JSON.parse(xhttp.responseText);
             var noinv = true;
+            var indx = 0;
             $.each(data, function(i, o) {
                 if (o.cogs_attacking != 'None' && Math.round(o.remaining_time)!=0) {
                     noinv = false;
-                    var newElement = `<section id="${i%2!=0?'card-left':'card-right'}">
+                    var newElement = `<section id="${indx%2==0?'card-left':'card-right'}">
 <img draggable="false" class="cog" src="https://toonhq.org/static/2.4.3/img/cogs/${o.cogs_attacking.toLowerCase().replace(/ /g,'_')}.png">
                 <h2>${o.cogs_attacking}</h2>
                 <h3>${o.name}</h3>
                 <p>${o.count_defeated}/${o.count_total} Cogs | <b>${Math.round(o.remaining_time/60)}</b> minutes remaining</p>
  </section>`;
                     element.insertAdjacentHTML('beforeend', newElement);
+                    indx++;
                 }
             });
             if (noinv) 
