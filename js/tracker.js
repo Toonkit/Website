82312b16569f72fb924846cd9d74187c342dff4d
Bin <jpahman2003@gmail.com>
2018-07-18 19:20:33 -0500

    Update tracker.js

--- js/tracker.js
+++ js/tracker.js
@@ -1,15 +1,16 @@
+var xhttp = new XMLHttpRequest();
+             xhttp.onreadystatechange = function() {
+                 if (this.readyState == 4 && this.status == 200) {
+                     var data = JSON.parse(xhttp.responseText);
+                    $("#curinv")[0].html=data;
+                 };
+
 $(document).ready(function() {  
 document.getElementById('curinv').innerHTML='trying';
 updateInvasions();
 });
 
   function updateInvasions() {
-             var xhttp = new XMLHttpRequest();
-             xhttp.onreadystatechange = function() {
-                 if (this.readyState == 4 && this.status == 200) {
-                     var data = JSON.parse(xhttp.responseText);
-                    $("#curinv")[0].html=data;
-                 };
                document.getElementById('curinv').innerHTML='opening';
                  xhttp.open("GET", "https://corporateclash.net/api/v1/districts", true);
                document.getElementById('curinv').innerHTML='sending';
