eff611d0ed977cc34c2101272cd31d1a56281731
Bin <jpahman2003@gmail.com>
2018-07-18 19:36:15 -0500

    Update tracker.js

--- js/tracker.js
+++ js/tracker.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
             $.each(data, function(i, o) {
                 if (o.cogs_attacking != 'None') {
                     var newElement = `<section id="${i%2==0?'card-left':'card-right'}">
-<img draggable="false" src="https://toonhq.org/static/2.4.3/img/cogs/${o.cogs_attacking}.png">
+<img draggable="false" src="https://toonhq.org/static/2.4.3/img/cogs/${o.cogs_attacking.toLowerCase()}.png">
                 <h2>${o.cogs_attacking}</h2>
                 <h3>${o.name}</h3>
                 <p>${o.count_defeated}/${o.count_total} Cogs | <b>${Math.round(o.remaining_time/60)}</b> minutes remaining</p>
@@ -18,13 +18,10 @@ $(document).ready(function() {
             });
         }
     };
-    document.getElementById('curinv').innerHTML = 'trying';
     updateInvasions();
 });
 
 function updateInvasions() {
-    document.getElementById('curinv').innerHTML = 'opening';
     xhttp.open("GET", "https://corporateclash.net/api/v1/districts", true);
-    document.getElementById('curinv').innerHTML = 'sending';
     xhttp.send();
 }
