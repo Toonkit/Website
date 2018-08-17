229230b779be05f0ba42ca08e2a961954eb35642
zzzachzzz <zach1rosen@gmail.com>
2018-08-17 12:44:49 -0600

    Changes for Heroku
    Aside from the various config files, the primary change in preparation for Heroku is migrating to PostgreSQL. For testing, the sqlite.db file is still included, so that installing Postgres is not necessary for testing.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn toonkit:app --log-file -
--- toonkit/__init__.py
+++ toonkit/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' # Testing DB (SQLite)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI') # Production DB (PostgreSQL)
 db = SQLAlchemy(app)
 
 from toonkit.routes.main import main
