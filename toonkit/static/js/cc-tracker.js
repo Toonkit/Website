dae6504d0217c75fe967a0d9c26e5a6a8a723ea4
zzzachzzz <zach1rosen@gmail.com>
2018-08-30 21:37:35 +0000

    CC Invasion Tracker

--- toonkit/static/js/cc-tracker.js
+++ toonkit/static/js/cc-tracker.js
@@ -4,10 +4,6 @@ $(document).ready(function() {
     if ($("#curinv")[0]) {
         initInvasions();
         updateStats();
-        setInterval(() => updateStats(), 60000);
-        $(".cog").on("error", function() { // Use Cog silhouette image if regular image missing
-            $(this).attr('src', '192.168.1.34:5000/static/images/cogs/1_unknown_cog.png');
-        }
     }
 });
 
@@ -15,6 +11,7 @@ function updateStats() {
     $("#curinv").html('');
     xhttp.open("GET", "https://corporateclash.net/api/v1/districts?_=" + new Date().getTime(), true);
     xhttp.send();
+    setInterval(() => updateStats(), 60000);
 }
 
 function initInvasions() {
@@ -28,20 +25,28 @@ function initInvasions() {
                 if (o.cogs_attacking != 'None' && Math.round(o.remaining_time)!=0) {
                     noinv = false;
                     var newElement =
-                        `<div id="${indx%2==0?'card-left':'card-right'}">
-                          <img draggable="false" class="cog" src="192.168.1.34:5000/static/images/cogs/${o.cogs_attacking.toLowerCase().replace(/ /g,'_')}.png">
+                        `<div class="cog-card">
+                          <img draggable="false" class="cog" src="http://100.115.92.197:5000/static/images/cogs/${o.cogs_attacking.toLowerCase().replace(/ /g,'_')}.png">
                           <h2>${o.cogs_attacking}</h2>
                           <h3>${o.name}</h3>
                           <p style="font-size:12px">${o.count_total-o.count_defeated}/${o.count_total} Cogs | <b>${Math.round(o.remaining_time/60)}</b> minutes remaining</p>
                         </div>`;
-                    element.insertAdjacentHTML('beforeend', newElement);
+                    for (i=0; i < 7; i++) {
+                        element.insertAdjacentHTML('beforeend', newElement);
+                    }
                     indx++;
                 }
             });
-            if (noinv) 
+            if (noinv) {
                 $("#noinv").show();
-            else 
+            }
+            else {
                 $("#noinv").hide();
+                // Use Cog silhouette image if regular image missing
+                $(".cog").on("error", function() {
+                    $(this).attr('src', 'http://100.115.92.197:5000/static/images/cogs/1_unknown_cog.png');
+                });
+            }
         }
-    };
+    }
 }
